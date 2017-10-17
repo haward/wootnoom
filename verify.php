cbe202c42a428ef4ccd4f91cba638d857b3f209b
songwoot <songwoot@gmail.com>
2017-10-17 15:40:05 +0700

    add verify

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = '26143a37dd7ee33fe3a8e8460f743e7e';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
