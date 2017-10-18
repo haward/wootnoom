393b807eba38d0784dd160d4088e122639c8240f
songwoot <songwoot@gmail.com>
2017-10-18 12:13:14 +0700

    add more keyword

--- configuration.php
+++ configuration.php
@@ -7,18 +7,18 @@ $CONFIGURATION =  array
 		'สถานที่',
 		'แผนที่',
 		'จัดงาน',
+		'แต่งที่',
 	),
 	'time' => array(
 		'time',
 		'เวลา',
+		'เริ่มงาน',
 	),
 	'picture' => array(
 		'picture',
 		'รูป',
 		'ภาพ',
 		'image',
+		'รุปภาพ',
 	),
 );
-
-
-
